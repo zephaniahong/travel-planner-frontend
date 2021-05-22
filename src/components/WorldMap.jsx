46aa6e13e265ac6053ba3bdffe47f424b3c86643
Zephaniah <zealorez@gmail.com>
2021-05-22 17:00:21 +0800

    add hover and click states

--- src/components/WorldMap.jsx
+++ src/components/WorldMap.jsx
@@ -1,8 +1,24 @@
 import { VectorMap } from "@south-paw/react-vector-maps";
 import worldMapJson from "./worldMapJson.js";
+import React, { useState } from "react";
 
 const WorldMap = () => {
-  return <VectorMap {...worldMapJson} />;
+  const [hovered, setHovered] = useState("None");
+  const [clicked, setClicked] = useState("None");
+
+  const layerProps = {
+    onMouseEnter: ({ target }) => setHovered(target.attributes.name.value),
+    onMouseLeave: ({ target }) => setHovered("None"),
+    onClick: ({ target }) => setClicked(target.attributes.name.value),
+  };
+
+  return (
+    <React.Fragment>
+      <VectorMap {...worldMapJson} layerProps={layerProps} />
+      <p>Hovered: {hovered && <code>{hovered}</code>}</p>
+      <p>Clicked: {clicked && <code>{clicked}</code>}</p>
+    </React.Fragment>
+  );
 };
 
 export default WorldMap;
