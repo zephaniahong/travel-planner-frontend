e9539ad56766cc6ba02eeaeeb27bfb70ccdb79cf
Zephaniah <zealorez@gmail.com>
2021-05-22 21:30:10 +0800

    include country trips

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {PlanningProvider} from './store'
 import Home from './components/Home.jsx'
+import CountryTrips from './components/CountryTrips'
 function App() {
   return (
     <PlanningProvider>
     <div className="container">
       <Home/>
+      <CountryTrips/>
     </div>
     </PlanningProvider>
   );
