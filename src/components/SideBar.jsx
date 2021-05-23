ef7fe3c43e940e9209d4951084c134e87aab8ca4
Jchu4 <jerome.chuame@gmail.com>
2021-05-23 22:48:13 +0800

    Add basic layout of sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 // import PlusCircleIcon from "./SideBar.jsx";
+import { Container, Row, Col } from "react-bootstrap";
 import Scrollspy from "react-scrollspy";
 
 const SideNav = styled.nav`
   width: 50rem;
-  height: 100vh;
+  height: 100%;
   position: absolute;
   background-color: black;
   padding: 0 30px;
@@ -41,28 +42,69 @@ export default function SideBar() {
       <SideNav>
         <StyledNavList>
           <StyledNavItem>
-            <h5 className="lead">Filter</h5>
+            <h5 className="lead">Filters</h5>
           </StyledNavItem>
-          <div>
-            <section id="section-1">section 1</section>
-            <section id="section-2">section 2</section>
-            <section id="section-3">section 3</section>
-          </div>
-
-          <Scrollspy
-            items={["section-1", "section-2", "section-3"]}
-            currentClassName="is-current"
-          >
-            <li>
-              <a href="#section-1">section 1</a>
-            </li>
-            <li>
-              <a href="#section-2">section 2</a>
-            </li>
-            <li>
-              <a href="#section-3">section 3</a>
-            </li>
-          </Scrollspy>
+          <Container>
+            <Row>
+              <Col md="3">
+                {" "}
+                <Scrollspy
+                  items={[
+                    "section1",
+                    "section2",
+                    "section3",
+                    "section4",
+                    "section5",
+                  ]}
+                  currentClassName="active"
+                  offset={-100}
+                >
+                  <li>
+                    <a href="#section1">Section 01</a>
+                  </li>
+                  <li>
+                    <a href="#section2">Section 02</a>
+                  </li>
+                  <li>
+                    <a href="#section3">Section 03</a>
+                  </li>
+                  <li>
+                    <a href="#section4">Section 04</a>
+                  </li>
+                  <li>
+                    <a href="#section5">Section 05</a>
+                  </li>
+                </Scrollspy>
+              </Col>
+              <Col md="9">
+                <section id="section1">
+                  <h2 className="text-light text-right mr-5 mb-5">
+                    Food Card A
+                  </h2>
+                </section>
+                <section id="section2">
+                  <h2 className="text-light text-right mr-5 mb-5">
+                    Food Card B
+                  </h2>
+                </section>
+                <section id="section3">
+                  <h2 className="text-light text-right mr-5 mb-5">
+                    Trip Card A
+                  </h2>
+                </section>
+                <section id="section4">
+                  <h2 className="text-light text-right mr-5 mb-5">
+                    Site Card A
+                  </h2>
+                </section>
+                <section id="section5">
+                  <h2 className="text-light text-right mr-5 mb-5">
+                    Food Card C
+                  </h2>
+                </section>
+              </Col>
+            </Row>
+          </Container>
         </StyledNavList>
       </SideNav>
     </div>
