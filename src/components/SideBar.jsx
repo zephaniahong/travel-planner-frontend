d900abe387c883fa2821350557d72158ac968396
Jchu4 <jerome.chuame@gmail.com>
2021-05-23 19:38:32 +0800

    Add Plus Circle icon svg

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import PlusCircleIcon from "./SideBar.jsx";
 
 const StyledNav = styled.nav`
   width: 15rem;
@@ -15,6 +16,10 @@ const StyledNavList = styled.ul`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  &:hover {
+    background: darkgrey;
+  }
 `;
 const StyledNavItem = styled.li`
   display: flex;
@@ -22,10 +27,18 @@ const StyledNavItem = styled.li`
   height: 5rem;
   color: white;
   text-decoration: none;
+
+  &:hover {
+    background: darkgrey;
+  }
 `;
 
 const StyledNavLinks = styled.a`
   text-decoration: none;
+
+  &:hover {
+    background: darkgrey;
+  }
 `;
 
 export default function SideBar() {
@@ -34,7 +47,8 @@ export default function SideBar() {
       <StyledNav>
         <StyledNavList>
           <StyledNavItem>
-            <a href="#">Home</a>
+            <StyledNavLinks>Home</StyledNavLinks>
+            <PlusCircleIcon />
           </StyledNavItem>
         </StyledNavList>
       </StyledNav>
