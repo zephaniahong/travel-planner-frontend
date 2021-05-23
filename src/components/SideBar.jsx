8e2d0aea9edf969005ef649d3c08ef8940ea75be
Jchu4 <jerome.chuame@gmail.com>
2021-05-23 20:57:32 +0800

    Work still in progress for scrollspy section for sidebar.

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 // import PlusCircleIcon from "./SideBar.jsx";
-import ScrollSpy from "react-scrollspy";
+import Scrollspy from "react-scrollspy";
 
 const SideNav = styled.nav`
-  width: 15rem;
+  width: 50rem;
   height: 100vh;
   position: absolute;
   background-color: black;
@@ -43,6 +43,26 @@ export default function SideBar() {
           <StyledNavItem>
             <h5 className="lead">Filter</h5>
           </StyledNavItem>
+          <div>
+            <section id="section-1">section 1</section>
+            <section id="section-2">section 2</section>
+            <section id="section-3">section 3</section>
+          </div>
+
+          <Scrollspy
+            items={["section-1", "section-2", "section-3"]}
+            currentClassName="is-current"
+          >
+            <li>
+              <a href="#section-1">section 1</a>
+            </li>
+            <li>
+              <a href="#section-2">section 2</a>
+            </li>
+            <li>
+              <a href="#section-3">section 3</a>
+            </li>
+          </Scrollspy>
         </StyledNavList>
       </SideNav>
     </div>
