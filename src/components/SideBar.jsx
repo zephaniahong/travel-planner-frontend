916e66954ca7c4a498ad44d803928e502c943832
Jchu4 <jerome.chuame@gmail.com>
2021-05-23 20:37:37 +0800

    Add react-scrollspy library

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import styled from "styled-components";
-import PlusCircleIcon from "./SideBar.jsx";
+// import PlusCircleIcon from "./SideBar.jsx";
+import ScrollSpy from "react-scrollspy";
 
-const StyledNav = styled.nav`
+const SideNav = styled.nav`
   width: 15rem;
   height: 100vh;
-  position: fixed;
+  position: absolute;
   background-color: black;
+  padding: 0 30px;
+  transition: all 1s;
 `;
 
 const StyledNavList = styled.ul`
@@ -16,42 +19,32 @@ const StyledNavList = styled.ul`
   display: flex;
   flex-direction: column;
   align-items: center;
-
-  &:hover {
-    background: darkgrey;
-  }
 `;
 const StyledNavItem = styled.li`
+  list-style: none;
   display: flex;
   align-items: center;
   height: 5rem;
   color: white;
   text-decoration: none;
-
-  &:hover {
-    background: darkgrey;
-  }
 `;
 
 const StyledNavLinks = styled.a`
+  list-style: none;
   text-decoration: none;
-
-  &:hover {
-    background: darkgrey;
-  }
+  color: white;
 `;
 
 export default function SideBar() {
   return (
     <div>
-      <StyledNav>
+      <SideNav>
         <StyledNavList>
           <StyledNavItem>
-            <StyledNavLinks>Home</StyledNavLinks>
-            <PlusCircleIcon />
+            <h5 className="lead">Filter</h5>
           </StyledNavItem>
         </StyledNavList>
-      </StyledNav>
+      </SideNav>
     </div>
   );
 }
