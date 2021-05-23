1567ef2a5e0eeb74509c3027822707b2ed8d2e79
Zephaniah <zealorez@gmail.com>
2021-05-23 19:59:59 +0800

    change div from container to container-fluid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {PlanningProvider} from './store'
 import Home from './components/Home.jsx'
+import CountryTrips from './components/CountryTrips.jsx'
 function App() {
   return (
     <PlanningProvider>
-    <div className="container">
+    <div className="container-fluid">
       <Home/>
+      <CountryTrips/>
     </div>
     </PlanningProvider>
   );
