8f7d0bd6791285a9557ec9cc7110c090e0f1aafc
Jchu4 <jerome.chuame@gmail.com>
2021-05-24 19:18:08 +0800

    Refactor sidebar code. Add buttons and css for button and sidebar animation

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,16 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-// import PlusCircleIcon from "./SideBar.jsx";
+import PlusCircleIcon from "./PlusCircleIcon.jsx";
 import TripCards from "./TripCards.jsx";
-
-const SideNav = styled.nav`
-  width: 50rem;
-  height: 100%;
-  position: absolute;
-  background-color: black;
-  padding: 0 30px;
-  transition: all 1s;
-`;
 
 const StyledNavList = styled.ul`
   list-style: none;
@@ -20,6 +11,7 @@ const StyledNavList = styled.ul`
   flex-direction: column;
   align-items: center;
 `;
+
 const StyledNavItem = styled.li`
   list-style: none;
   display: flex;
@@ -29,23 +21,25 @@ const StyledNavItem = styled.li`
   text-decoration: none;
 `;
 
-const StyledNavLinks = styled.a`
-  list-style: none;
-  text-decoration: none;
-  color: white;
-`;
-
 export default function SideBar({ children }) {
+  const [showNav, setShowNav] = useState(false);
+
   return (
-    <div>
-      <SideNav>
-        <StyledNavList>
-          <StyledNavItem>
-            <h5 className="lead">Filters</h5>
-          </StyledNavItem>
-          <div>{children}</div>
-        </StyledNavList>
-      </SideNav>
-    </div>
+    <>
+      <header className="sidebar-btn">
+        <PlusCircleIcon setShowNav={setShowNav} showNav={showNav} />
+      </header>
+
+      <div show={showNav}>
+        <div className={showNav ? "sidenav active" : "sidenav"}>
+          <StyledNavList>
+            <StyledNavItem>
+              <h5 className="lead">Filters</h5>
+            </StyledNavItem>
+            <div>{children}</div>
+          </StyledNavList>
+        </div>
+      </div>
+    </>
   );
 }
