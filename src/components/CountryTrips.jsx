f44a1a80c698672ad1b9e0fa47bd39378bb6d115
Jchu4 <jerome.chuame@gmail.com>
2021-05-24 22:29:01 +0800

    Trying to get trips from DB. Continue in the morning.

--- src/components/CountryTrips.jsx
+++ src/components/CountryTrips.jsx
@@ -1,12 +1,24 @@
+import React, { useEffect, useContext } from "react";
 import TripCards from "./TripCards.jsx";
+import { getTrips, PlanningContext } from "../store.js";
 import CountryMap from "./CountryMap.jsx";
 import SideBar from "./SideBar.jsx";
 
 const CountryTrips = () => {
+  const { store, dispatch } = useContext(PlanningContext);
+  const { trips } = store;
+
+  useEffect(() => {
+    getTrips(dispatch);
+  }, []);
+
   return (
     <div>
       <SideBar>
         <h1>Hello World</h1>
+        {trips.map((trip) => {
+          return <h2>trip.hotelDetails</h2>;
+        })}
       </SideBar>
       <CountryMap />
     </div>
