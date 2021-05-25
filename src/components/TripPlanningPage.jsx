c4c337e26baf73f7095c9fb83b742731110b170e
Zephaniah <zealorez@gmail.com>
2021-05-25 11:05:42 +0800

    add google search to side bar

--- src/components/TripPlanningPage.jsx
+++ src/components/TripPlanningPage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { Container, Col, Row } from "react-bootstrap";
 import SideBar from "./SideBar.jsx";
+import GoogleSearch from "./GoogleSearch.jsx";
 
 export default function TripPlanningPage() {
-  return <SideBar />;
+  return (
+    <SideBar>
+      <GoogleSearch />
+    </SideBar>
+  );
 }
