39f4e30266c286c71a7f50e72f274e6824ac5f65
Jchu4 <jerome.chuame@gmail.com>
2021-05-25 12:04:24 +0800

    Add store, dispatch and dependency functions to alltrips

--- src/components/AllTrips.jsx
+++ src/components/AllTrips.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
+import { PlanningContext } from "../store.js";
 import { Container, Row, Col } from "react-bootstrap";
 
 export default function AllTrips() {
+  const { store, dispatch } = useContext(PlanningContext);
+
+  useEffect(() => {}, []);
+
   return (
     <Container fluid>
-      <Row>
+      <Row className="mt-4">
         <Col>
           <h1 className="text-center">All Trips</h1>
         </Col>
+      </Row>
+      <Row>
+        <Col>TripCards</Col>
       </Row>
     </Container>
   );
