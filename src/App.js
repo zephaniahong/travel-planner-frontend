004425736e468c5f1a698d4b98cbb7dff651cba4
Jchu4 <jerome.chuame@gmail.com>
2021-05-27 20:08:30 +0800

    Base router code, todo: refactor to utitlise store. Move some of the code from App to lower component to utilise store

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, 
   Switch, 
   Route, 
 } from 'react-router-dom';
-import {PlanningProvider} from './store';
+import {PlanningProvider} from './store.js';
 import Home from './components/Home.jsx';
 import Navbar from './components/Navbar.jsx';
 import CountryTrips from './components/CountryTrips.jsx'
 import TripPlanningPage from './components/TripPlanningPage.jsx';
 import AllTrips from './components/AllTrips.jsx'
-// import SingleTrip from './components/SingleTrip.jsx'
+import SingleTrip from './components/SingleTrip.jsx'
 import { Container } from 'react-bootstrap';
 import './App.css'
+import axios from 'axios';
+
+const BACKEND_URL = 'http://localhost:3004';
+
 
 function App() {
+  const [selectedTripIdx, setSelectedTrip] = useState();
+  const [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${BACKEND_URL}/gettrips`)
+      .then(res => {
+        setTrips(res.data);
+      })
+  }, [])
+
+  const onDeepLink = (tripIdx) => {
+    setSelectedTrip(tripIdx);
+  };
+
+  const selectedTrip = trips[selectedTripIdx];
+
   return (
     <PlanningProvider>
       <Router>
@@ -26,7 +46,9 @@ function App() {
             <TripPlanningPage />
           </Route>
 
-          {/* <Route path={"/trip/:tripId"} component={SingleTrip} /> */}
+          <Route path="/trip/:tripId"  >
+            <SingleTrip selectedTrip={selectedTrip} onDeepLink={onDeepLink} />
+          </Route>
 
           <Route path="/alltrips">
             <AllTrips />
--- src/components/SingleTrip.jsx
+++ src/components/SingleTrip.jsx
@@ -1,24 +1,24 @@
-import React, { useContext } from "react";
-import { PlanningContext } from "./store.js";
+import React, { useContext, useEffect } from "react";
+// import { PlanningContext } from "../store.js";
+import { useParams } from "react-router-dom";
 
-export default function SingleTrip({ match }) {
-  const { store, dispatch } = useContext(PlanningContext);
-  const { trips } = store;
+export default function SingleTrip({ match, selectedTrip, onDeepLink }) {
+  // const { store, dispatch } = useContext(PlanningContext);
+  // const { trips } = store;
+  let { tripId } = useParams();
 
-  const singleTrip = trips.find((trip) => {
-    return parseInt(match.params.tripId) === trip.id;
-  });
+  // const singleTrip = trips.find((trip) => {
+  //   return parseInt(match.params.tripId) === trip.id;
+  // });
 
-  return (
-    <>
-      {match.isExact && (
-        <>
-          <h1>Hello Single Trip</h1>
-          {trips.map((trip) => {
-            return <p>{trip.id}</p>;
-          })}
-        </>
-      )}
-    </>
-  );
+  useEffect(() => {
+    if (!selectedTrip) {
+      onDeepLink(tripId);
+    }
+  }, []);
+
+  if (!selectedTrip) {
+    return <div>Jerome</div>;
+  }
+  return <>{selectedTrip.name}</>;
 }
